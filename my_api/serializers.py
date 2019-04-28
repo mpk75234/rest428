919dde0c4ba620f5e4f39767a8a0760a1ee8538b
Mike Kennedy <mikekennedy75234@gmail.com>
2019-04-28 16:14:36 -0500

    added ModelSerializer to make create() update() easy

--- my_api/serializers.py
+++ my_api/serializers.py
@@ -1,25 +1,32 @@
 from rest_framework import serializers
 from my_api.models import Verbos
 
-class VerbosSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    verb = serializers.CharField(max_length=50)
-    meaning = serializers.CharField(max_length=50)
-    language = serializers.CharField(max_length=50)
+class VerbosSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Verbos
+        fields = '__all__'
 
-    def create(self, validated_data):
-        """
-        Create and return a new 'Verbos' instance , given validated data
-        """
-        return Verbos.objects.create(**validated_data)
+# class VerbosSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     verb = serializers.CharField(max_length=50)
+#     meaning = serializers.CharField(max_length=50)
+#     language = serializers.CharField(max_length=50)
 
-    def update(self, instance, validated_data):
-        """
-        Update a Verbos instance
-        """
-        instance.verb = validated_data.get('verb', instance.verb)
-        instance.meaning = validated_data.get('meaning', instance.verb)
-        instance.language = validated_data.get('language', instance.language)
-        instance.save()
-        return instance
+#     def create(self, validated_data):
+#         """
+#         Create and return a new 'Verbos' instance , given validated data
+#         """
+#         return Verbos.objects.create(**validated_data)
 
+#     def update(self, instance, validated_data):
+#         """
+#         Update a Verbos instance
+#         """
+#         instance.verb = validated_data.get('verb', instance.verb)
+#         instance.meaning = validated_data.get('meaning', instance.verb)
+#         instance.language = validated_data.get('language', instance.language)
+#         instance.save()
+#         return instance
+
+#     def __str__(self):
+#         return self.verb
